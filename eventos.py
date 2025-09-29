91008b1e76667ecc2dfe08b6b6e5b2aac988972c
Maxi Benet <maximilianobenet@gmail.com>
2025-09-29 10:54:19 -0300

    correccion de codigo en eventos.py

--- eventos.py
+++ eventos.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 #leer el archivo con pandas
-df_unico_municipio = pd.read_csv("df_unico_municipios.csv")
+df_unico_municipio = pd.read_csv("df_limpio_municipio_sin_repeticiones.csv")
 
 #conteo de casos por evento
 conteo_eventos = df_unico_municipio["evento"].value_counts().reset_index()
 conteo_eventos.columns = ["evento", "cantidad"]
 
-#conteo_eventos.to_csv("conteo_eventos.csv", index=False, encoding="utf-8")
+conteo_eventos.to_csv("conteo_eventos.csv", index=False, encoding="utf-8")
 
 print(conteo_eventos)
